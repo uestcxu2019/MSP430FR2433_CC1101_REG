4e8b0a419e8e4ed870184f7650bd072f3fb57360
xuz <uestcxz@163.com>
2019-04-30 09:08:19 +0800

    MSP430FR2433与CC1101/CC115L无线通信

---
+++ BSP/bsp_spi.h
@@ -0,0 +1,34 @@
+/*
+ * bsp_spi.h
+ *
+ *  Created on: 2018年11月30日
+ *      Author: xu
+ *      使用USCI_B0的 SPI功能
+ */
+
+#ifndef BSP_BSP_SPI_H_
+#define BSP_BSP_SPI_H_
+
+#include "msp430fr2433.h"
+#include "bsp_delay.h"
+
+//CS引脚宏定义
+#define  SPI_CS_PIN			BIT0
+
+//CLK引脚宏定义
+#define  SPI_CLK_PIN		BIT1
+
+//MOSI引脚宏定义
+#define  SPI_MOSI_PIN		BIT2
+
+//MISO引脚宏定义
+#define  SPI_MISO_PIN		BIT3
+
+
+void SPI_CS_HIGH(void);
+void SPI_CS_LOW(void);
+
+void SPI_Init(void);
+uint8_t SPI_Send(uint8_t data);
+
+#endif /* BSP_BSP_SPI_H_ */
